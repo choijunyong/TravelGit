9116be25c51b1295170138c3819dca983906ba09
YEON <alphasuno214@gmail.com>
2018-05-08 21:42:46 +0900

    Auto stash before merge of "dev" and "origin/dev"

--- app/src/main/java/com/example/syl/travel/ui/LoginActivity.java
+++ app/src/main/java/com/example/syl/travel/ui/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.syl.travel.ui;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInstaller;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethod;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,6 +20,15 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
 
 import org.json.JSONObject;
 
@@ -27,6 +39,8 @@ public class LoginActivity extends AppCompatActivity {
     static String username;
 
     private CallbackManager callbackManager;
+    private SessionCallback callback;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +72,23 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
                 LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        ///카카오
+        Button kakaoLoginButton = (Button) findViewById(R.id.kkLoginButton);
+
+        callback = new SessionCallback();
+        getCurrentSession().addCallback(callback);
+        getCurrentSession().checkAndImplicitOpen();
+
+        /*************************************************************카카오톡 로그아웃 요청**/
+        //한번 로그인이 성공하면 세션 정보가 남아있어서 로그인창이 뜨지 않고 바로 onSuccess()메서드를 호출합니다.
+        //테스트 하시기 편하라고 매번 로그아웃 요청을 수행하도록 코드를 넣었습니다 ^^
+        UserManagement.requestLogout(new LogoutResponseCallback() {
+            @Override
+            public void onCompleteLogout() {
+                //로그아웃 성공 후 하고싶은 내용 코딩 ~
             }
         });
     }
